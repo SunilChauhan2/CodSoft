1144c3f3dcfe80962aa7dcf9ca11922b97c1170d
SunilChauhan2 <sunilchauhan27c@gmail.com>
2024-10-09 14:49:23 +0530

    Added calculator code

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+#Heading
+print("               MINI CALCULATOR           ")
+
+#Input Numbers
+A=int(input("Enter first number: "))  
+B=int(input("Enter second number: "))
+
+#function for calculation
+def operations (A,B):
+    if (x== "+"):
+        print(A+B)
+    elif(x== "-"):
+        print(A-B)
+    elif(x== "/"):
+        print(A/B)
+    elif(x== "*"):
+        print(A*B)
+    else:
+        print("INVALID INPUT")
+
+#Operation 
+x=input("Chose Operation (+,-,/,*) : ")
+#function call
+operations(A,B)
+print("Thanks For Calculating 45:)")
